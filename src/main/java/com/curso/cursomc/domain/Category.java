4992a1f21ad3eabeec80d892ec1623ffaa7a0200
Luan Leal <luanlealboni@gmail.com>
2024-03-12 13:43:35 -0300

    :sparkles: FEAT: add h2 database

--- src/main/java/com/curso/cursomc/domain/Category.java
+++ src/main/java/com/curso/cursomc/domain/Category.java
@@ -1,5 +1,9 @@
 package com.curso.cursomc.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,10 +12,13 @@ import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
+@Entity
 public class Category implements Serializable {
     @Serial
     private  static  final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Integer id;
     private  String name;
 
