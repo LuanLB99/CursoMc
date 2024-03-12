99ea996c9afefd8e28f88bcc2063ff33e9a866c5
Luan Leal <luanlealboni@gmail.com>
2024-03-12 14:24:42 -0300

    :sparkles: FEAT: add category instantiation

--- src/main/java/com/curso/cursomc/CursomcApplication.java
+++ src/main/java/com/curso/cursomc/CursomcApplication.java
@@ -1,12 +1,31 @@
 package com.curso.cursomc;
 
+import com.curso.cursomc.domain.Category;
+import com.curso.cursomc.repositories.CategoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	final CategoryRepository categoryRepository;
+
+	public CursomcApplication(CategoryRepository categoryRepository) {
+		this.categoryRepository = categoryRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 }
