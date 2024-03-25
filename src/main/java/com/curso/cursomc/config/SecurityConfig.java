8f2c20bb491cdc28fcb8f4c9110f2580a142118e
Luan Leal <luanlealboni@gmail.com>
2024-03-25 16:03:37 -0300

    :wrench: CHORE: Inicial spring-security configuration

---
+++ src/main/java/com/curso/cursomc/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.curso.cursomc.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Autowired
+    private Environment env;
+    private static final String[] PUBLIC_MATCHERS = {
+            "/h2-console/**",
+    };
+
+    private static final String[] PUBLIC_MATCHERS_GET = {
+            "/products/**",
+            "/categories/**"
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+
+        http.cors(withDefaults())
+                .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(PUBLIC_MATCHERS).permitAll().requestMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+                .anyRequest().authenticated());
+        return http.build();
+    }
+
+/*    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }*/
+}
--- src/main/java/com/curso/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/curso/cursomc/services/AbstractEmailService.java
@@ -8,11 +8,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
 import java.util.Date;
 
+@Service
 public abstract class AbstractEmailService implements EmailService{
     @Value("${default.sender}")
     private String sender;
